ffbc98aefdcc44dba46051787b03ccf7e938d79c
andmalt <andrea.maltempi@gmail.com>
2021-12-27 17:54:13 +0100

    adjusted style

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,25 +8,25 @@ const Main = () => {
     return (
         <div className='row text-center'>
             <div className="col-12">
-                <h1 className='p-2' id='t-m'>Prossimi Incontri</h1>
+                <h1 className='p-3 mt-2' id='t-m'>Prossimi Incontri</h1>
             </div>
             <div className="col-12 p-2 my-3">
                 <div className="row">
                     {
-                        meets.map(el => {
+                        person.map(el => {
                             const {id,name,phrase,image} = el;
                             return(
-                                <div key={id} className="col-12">
-                                    <div className="row">
-                                        <div className="col-12 col-md-3">
-                                            <img className="img-fluid" src={image} alt="" />
+                                <div key={id} className="col-12 card_person shadow">
+                                    <div className="row justify-content-center align-items-center">
+                                        <div className="col-7 col-sm-5 col-md-3 div-img">
+                                            <img className="img-fluid" src={image} alt={`${name} immagine`} />
                                         </div>
                                         <div className="col-12 col-md-7">
                                             <h5>{name}</h5>
                                             <p>{phrase}</p>
                                         </div>
-                                        <div className="col-12 col-md-2">
-                                            <button></button>
+                                        <div className="col-12 col-md-2 p-3">
+                                            <button className='btn btn-danger px-4'>X</button>
                                         </div>
                                     </div>
                                 </div>
--- src/data/data.js
+++ src/data/data.js
@@ -16,7 +16,7 @@ export const meets =[
         id: 3,
         name: 'Michael Jordan',
         phrase: 'Non preoccuparti di fallire. Preoccupati di non averci provato.',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/a/ae/Michael_Jordan_in_2014.jpg'
+        image: 'https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F603811ed0f3061f16a7be88c%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D598%26cropX2%3D2175%26cropY1%3D97%26cropY2%3D1675'
     },
     {
         id: 4,
